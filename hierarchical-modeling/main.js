399c662981a2b5426e71ce1b6fd175b9562c842b
Erdem <ege.marasli@ug.bilkent.edu.tr>
2019-11-29 05:53:26 +0300

    Animation is done

--- hierarchical-modeling/main.js
+++ hierarchical-modeling/main.js
@@ -197,9 +197,13 @@ function clearFrame(){
 
 function playAnimation(){
     if (animationCount <  keyFrames.length){
+        console.log("asda");
         limbs = deepCopy(keyFrames[animationCount]);
         if (keyFrames.length - 1 > animationCount){
             interpolate(keyFrames[animationCount + 1]);
+        }
+        else{
+            animationCount = 0;
         }
     }
 }
@@ -443,6 +447,12 @@ function bindEvents() {
             reader.onload = () => {
                 const content = reader.result;
                 const data = JSON.parse(content);
+                keyFrames = data;
+                for (let i = 0; i < keyFrames.length; i++){
+                    for (let j = 0; j < keyFrames[i].length; j++){
+                        keyFrames[i][j].transform.matrix = true;
+                    }
+                }
                 
 
             }
@@ -475,7 +485,7 @@ function bindEvents() {
     });
 
     $('#saveButton').click(() => {
-        downloadObjectAsJson({}, "data");
+        downloadObjectAsJson(keyFrames, "animation");
     });
 
     $('#loadButton').click(() => {
